ef9612a644fe304db8e0ec846b644b433d334b11
rael <raelga@gmail.com>
2016-06-13 00:53:24 +0200

    Smarter hello, replies to greetings and nights.

--- memory/hello.py
+++ memory/hello.py
@@ -6,8 +6,22 @@
  Tested on:     Python 3 / OS X 10.11.5
 """
 import re
+import random
 
+greetings = ['Buenos días', 'Bon dia', 'Good Morning',
+                'Egun on', 'Morning', 'Buon giorno',
+                'Bonjour', 'Bos días', 'Buenos días arsa',
+                'Hola miarma', 'Goede morgen', 'Bom dia',
+                'Días buenos a ti también'] 
+
+nights = ['Buenas noches', 'Bona nit', 'Good night',
+              'Nanit', 'Gute natch', 'Buonna note',
+              'Bonne nuit','Goedenacht', 'Boa noite']
 
 def hear(words):
-    return "Tell me more."
 
+    if any(greeting.lower() in words.lower() for greeting in greetings):
+        return random.choice(greetings)
+
+    if any(night.lower() in words.lower() for night in nights):
+        return random.choice(nights)
