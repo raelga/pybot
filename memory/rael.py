cc39c1389bc8e9c22d657b618287bd8a401becf7
rael <raelga@gmail.com>
2016-06-16 11:35:10 +0200

    Added rael.py to mention me when someone says my name.

---
+++ memory/rael.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+ hellp.py       Be polite.
+ Author:        Rael Garcia <self@rael.io>
+ Date:          06/2016
+ Tested on:     Python 3 / OS X 10.11.5
+"""
+import re
+import sys
+import random
+
+def hear(words):
+    if re.search( r'.*\srael\s.*', words, re.I|re.M):
+        return "@raelga"
+
+def main(argv):
+    if len(sys.argv)>1:
+        print(hear(' '.join(sys.argv)))
+    else:
+        print('I heard nothing.')
+
+if __name__ == "__main__":
+    main(sys.argv)
