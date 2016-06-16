de80cfdc9b020d0ea2ab37534aa1e6f059b20eb8
rael <raelga@gmail.com>
2016-06-16 12:26:23 +0200

    Typo.

--- memory/hello.py
+++ memory/hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
- hellp.py       Be polite.
+ hello.py       Be polite.
  Author:        Rael Garcia <self@rael.io>
  Date:          06/2016
  Tested on:     Python 3 / OS X 10.11.5
