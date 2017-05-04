22f3484e3f666d2188cec97f1fe5464ce6f295c1
Rael Garcia <raelga@gmail.com>
2017-05-04 21:11:56 +0200

    Add usage to psntrophies.

--- pybot/memory/psntrophies.py
+++ pybot/memory/psntrophies.py
@@ -48,6 +48,8 @@ def hear(words):
     data = re.search(r'(^|.*)trophies ([A-Za-z0-9_-]+).*', words, re.I | re.M)
     if data:
         return psntrophies(data.groups()[1])
+    else:
+        return "Usage: /trophies PSNID"
 
     data = re.search(r'^perfil ([A-Za-z0-9_-]+).*', words, re.I | re.M)
     if data:
