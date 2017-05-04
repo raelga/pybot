192d344381d9a70e155c3a519074a091c239fded
Rael Garcia <raelga@gmail.com>
2017-05-04 21:58:49 +0200

    Improve psntrophies message handler.

--- pybot/memory/psntrophies.py
+++ pybot/memory/psntrophies.py
@@ -8,11 +8,12 @@
 
 from urllib import request as r
 import json
-import re
 import sys
+import time
+from pybot.common.action import Action
 
 
-def psntrophies(psnid):
+def __psntrophies(psnid):
 
     url = "https://io.playstation.com/playstation/psn/public/trophies/?onlineId="
     headers = {"Referer": "https://www.playstation.com/en-us/my/compare-game-trophies",
@@ -27,7 +28,7 @@ def psntrophies(psnid):
     games = len(data['list'])
     platinum = 0
     completed = 0
-    info = "Recent games played\n"
+    info = "Recent games played:\n\n"
 
     for g in range(games):
         if g <= 5:
@@ -38,25 +39,71 @@ def psntrophies(psnid):
         if data['list'][g]['trophies']['platinum'] == 1:
             platinum += 1
 
-    summary = psnid + "\n" + repr(platinum) + \
+    summary = "*" + psnid + "*\n" + repr(platinum) + \
         " platinums (" + repr(completed) + " 100%s)"
 
     return summary + "\n\n" + info
 
 
-def hear(words):
-    data = re.search(r'(^|.*)trophies ([A-Za-z0-9_-]+).*', words, re.I | re.M)
-    if data:
-        return psntrophies(data.groups()[1])
+def __usage(handler):
+    """Prints usage information with the handler command."""
+    return (
+        'Usage: *%s* _PSNID_'
+        % (handler)
+    )
 
-    data = re.search(r'^perfil ([A-Za-z0-9_-]+).*', words, re.I | re.M)
-    if data:
-        return psntrophies(data.groups()[0])
+
+def __mdsafe(text):
+    """Escape unsafe Markdown characters."""
+    return text.replace("_", r"\_")
+
+
+def ___message_handler(message):
+    """Parses the user message call the psnprofiles method if valid"""
+
+    words = message.text.split()
+
+    if not words:
+        return None
+
+    handler = words[0]
+
+    if len(words) < 2:
+        response = __usage(handler)
+    else:
+        response = __mdsafe(__psntrophies(words[1]))
+
+    return Action(
+        name='new_message',
+        target=message.chat.chat_id,
+        text=response,
+        markup='markdown'
+    )
+
+
+def perfil(message):
+    "This allows to respond to /perfil command."
+    return ___message_handler(message)
+
+
+def trophies(message):
+    "This allows to respond to /trohpies command."
+    return ___message_handler(message)
 
 
 def main(argv):
+    "This allows to execute the plugin in standalone mode"
     if len(sys.argv) > 1:
-        print(hear(' '.join(sys.argv)))
+
+        from pybot.common.message import Message
+        from pybot.common.user import User
+        from pybot.common.chat import Chat
+        user = User(1, 'foo', 'bar', 'rael')
+        chat = Chat(1, 'Console', 'Console')
+        message = Message(1, user, time.strftime,
+                          ' '.join(sys.argv[1:]), chat, None)
+        print(trophies(message))
+
     else:
         print('I heard nothing.')
 
