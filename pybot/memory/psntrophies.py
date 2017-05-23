d3738ab3fd4d3ddf482ad742dd2030c00c4c8fab
Rael Garcia <raelga@gmail.com>
2017-05-23 08:13:13 +0200

    Format the total number of trophies.

--- pybot/memory/psntrophies.py
+++ pybot/memory/psntrophies.py
@@ -59,7 +59,7 @@ def __psntrophies(psnid):
         level = data['curLevel']
 
         summary = [
-            "*%s* ⭐️ *%s* 🏅 %s" %
+            "*%s* ⭐️ *%s* - %s _trophies_" %
             (psnid, level, platinum + gold + silver + bronze),
             "",
             "🏆 %s platinums (%s 100%%s)" % (platinum, completed),
