dda61fc700fb2461916695be86e2ca8ec45713ab
Rael Garcia <raelga@gmail.com>
2017-06-30 21:45:20 +0200

    Change cat plugin behivaour.

--- pybot/memory/cats.py
+++ pybot/memory/cats.py
@@ -5,38 +5,27 @@
  Date:          06/2016
  Tested on:     Python 3 / OS X 10.11.5
 """
-import re
-import sys
+
 import random
 
 
-def send_a_cat(message):
+def _get_cat_gif():
     "Get a random cat from catapi"
 
     cat_apiurl = "http://thecatapi.com/api/images/get?format=src&type=gif"
-    cat_users = [329350632, 53693428, 13932196]
-
-    if message.user.user_id in cat_users:
-        if re.search(r'(^|\s)g[aA0-9]t.', message.text, re.I | re.M) \
-                or re.search(r'\\U0001F63[8-9A-F]', message.text, re.I | re.M) \
-                or re.search(r'(🐈|🐱|😺|😸|😹|😻|😼|😽|🙀|😿|😾)',
-                             message.text, re.I | re.M):
-            return cat_apiurl + "&timestamp=" + \
-                str(random.random()) + ".gif"
+    return cat_apiurl + "&timestamp=" + str(random.random()) + ".gif"
 
 
-def interact(message):
-    "Implements hear to receive the messages and execute the plugin logic"
-    return send_a_cat(message)
+def cat():
+    "Returns the groups menu for the command shortcut `groups`."
+
+    return _get_cat_gif()
 
 
-def main(argv):
+def main():
     "This allows to execute the plugin in standalone mode"
-    if len(argv) > 1:
-        print(hear(' '.join(sys.argv)))
-    else:
-        print('I heard nothing.')
+    print(cat())
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
